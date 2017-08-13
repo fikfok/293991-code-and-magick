b25864e291d6d1b17d83ecc6fb39f43f3a08f12a
fikfok <sadovenko.dmitry@gmail.com>
2017-08-14 00:25:08 +0300

    ИСправление замечаний

--- js/stat.js
+++ js/stat.js
@@ -1,59 +1,62 @@
 'use strict';
 
-var WHITE_COLOR = 'rgb(255, 255, 255)';
-var BLACK_COLOR = 'rgba(0, 0, 0, 0.7)';
-
-var scoreBoard = {
-  x: 100,
-  y: 10,
-  width: 420,
-  height: 270
-};
-var scoreBoardTitle = {
-  titles: ['Ура вы победили!', 'Список результатов:'],
-  horizontalAlign: 'start',
-  baseLine: 'hanging',
-  font: 'bold 16px PT Mono'
-};
-
-var histogram = {
-  height: 150,
-  barWidth: 40,
-  barMargin: 50,
-  barColorMe: 'rgba(255, 0, 0, 1)',
-  getBarColorOthers: function () {
-    return 'rgba(0, 0, ' + parseInt(Math.random() * 255, 10).toString() + ', 1)';
-  }
-};
-
-var drawRect = function (ctx, color, x, y, width, height) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, width, height);
-};
-
 window.renderStatistics = function (ctx, names, times) {
-  drawRect(ctx, BLACK_COLOR, scoreBoard.x + 10, scoreBoard.y + 10, scoreBoard.width, scoreBoard.height);
-  drawRect(ctx, WHITE_COLOR, scoreBoard.x, scoreBoard.y, scoreBoard.width, scoreBoard.height);
+  var WHITE_COLOR = 'rgb(255, 255, 255)';
+  var BLACK_COLOR = 'rgba(0, 0, 0, 0.7)';
 
-  ctx.fillStyle = BLACK_COLOR;
-  ctx.textAlign = scoreBoardTitle.horizontalAlign;
-  ctx.textBaseline = scoreBoardTitle.baseLine;
-  ctx.font = scoreBoardTitle.font;
-  ctx.fillText(scoreBoardTitle.title[0], scoreBoard.x + scoreBoard.width / 2 - ctx.measureText(scoreBoardTitle.title[0]).width / 2, scoreBoard.y + 10);
-  ctx.fillText(scoreBoardTitle.title[1], scoreBoard.x + scoreBoard.width / 2 - ctx.measureText(scoreBoardTitle.title[1]).width / 2, scoreBoard.y + 30);
+  var rect = {
+    x: 110,
+    y: 20,
+    width: 420,
+    height: 270
+  };
+
+  var histogram = {
+    height: 150,
+    barWidth: 40,
+    barMargin: 50,
+    barColorMe: 'rgba(255, 0, 0, 1)',
+    getBarColorOthers: function () {
+      return 'rgba(0, 0, ' + parseInt(Math.random() * 255, 10).toString() + ', 1)';
+    }
+  };
+
+  var drawRect = function (context, color, rectangle) {
+    context.fillStyle = color;
+    context.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+  };
+
+  var drawText = function (context, color, x, y, text) {
+    context.fillStyle = color;
+    context.textAlign = 'start';
+    context.textBaseline = 'hanging';
+    context.font = 'bold 16px PT Mono';
+    context.fillText(text, x, y);
+  };
+
+  var findMaxTime = function (array) {
+    return Math.max.apply(null, array);
+  };
+
+  var titles = ['Ура вы победили!', 'Список результатов:']
+
+  drawRect(ctx, BLACK_COLOR, rect);
+  rect.x -= 10;
+  rect.y -= 10;
+  drawRect(ctx, WHITE_COLOR, rect);
+  drawText(ctx, BLACK_COLOR, rect.x + rect.width / 2 - ctx.measureText(titles[0]).width / 2, rect.y + 10, titles[0]);
+  drawText(ctx, BLACK_COLOR, rect.x + rect.width / 2 - ctx.measureText(titles[1]).width / 2, rect.y + 30, titles[1]);
 
   var maxValue = findMaxTime(times);
-  for (var i = 0; i < names.length; i++) {
-    var x = histogram.barMargin + scoreBoard.x + (histogram.barWidth + histogram.barMargin) * i;
-    var barHeight = times[i] / maxValue * histogram.height;
-    drawRect(ctx, names[i] === 'Вы' ? histogram.barColorMe : histogram.getBarColorOthers(), x, scoreBoard.y + (histogram.height - barHeight) + 80, histogram.barWidth, barHeight);
-    ctx.fillStyle = BLACK_COLOR;
-    ctx.fillText(names[i], x, scoreBoard.y + scoreBoard.height - 25);
-    ctx.fillText(parseInt(times[i], 10), x, scoreBoard.y + (histogram.height - barHeight) + 55);
+  var arrLength = names.length;
+  rect.y = 250;
+  rect.width = histogram.barWidth;
+  for (var i = 0; i < arrLength; i++) {
+    rect.x += histogram.barWidth + ((i > 0) ? histogram.barMargin : 0);
+    rect.height = -1 * times[i] / maxValue * histogram.height;
+    var currentBarColor = (names[i] === 'Вы') ? histogram.barColorMe : histogram.getBarColorOthers();
+    drawRect(ctx, currentBarColor, rect);
+    drawText(ctx, BLACK_COLOR, rect.x, rect.y + 5, names[i]);
+    drawText(ctx, BLACK_COLOR, rect.x, rect.y + rect.height - 20, parseInt(times[i], 10));
   }
 };
-
-var findMaxTime = function (times) {
-  return Math.max.apply(null, times);
-};
-
